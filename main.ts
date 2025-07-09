a12ecc9691f1bd399715b7048596fe513cf4a6fd
mateuszm-arianelabs <mateusz.marcinkowski@arianelabs.com>
2025-07-09 09:52:57 +0200

    example TCO for SwapTokens

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import {createPublicClient, createWalletClient, formatUnits, http, parseEther} from 'viem'
+import {createPublicClient, createWalletClient, erc20Abi, formatUnits, http, parseEther, parseUnits} from 'viem'
 import {bsc} from 'viem/chains'
 import { encodeDeployData } from 'viem/utils'
 import { privateKeyToAccount } from 'viem/accounts'
@@ -9,6 +9,8 @@ import "dotenv/config"
 type Address = `0x${string}`
 
 const BNB_FACTORY_ADDRESS = "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73";
+const BNB_ROUTER_ADDRESS = "0x10ED43C718714eb63d5aA57B78B54704E256024E";
+const BNB_WETH_ADDRESS = "0x4DB5a66E937A9F4473fA95b1cAF1d1E1D62E29EA"
 const BNB_RPC = "https://bsc-rpc.publicnode.com"
 const PRIVATE_KEY = process.env.PRIVATE_KEY as Address;
 const PUBLIC_KEY = "0xACD0BD350355336c5537dE56250Ef01eD61e73eB"
@@ -29,7 +31,41 @@ const client = createWalletClient({
 const ARTIFACTS_PATH = "./artifacts/contracts/exchange-protocol/contracts/";
 
 async function estimateSwapContractsGas() {
-    const routerContractPath = path.join(ARTIFACTS_PATH, "PancakeFactory.sol/PancakeFactory.json")
+    const factoryContractPath = path.join(ARTIFACTS_PATH, "PancakeFactory.sol/PancakeFactory.json")
+    const factoryContractArtifact = await fs.readFile(factoryContractPath, "utf-8")
+
+    const factoryContract = JSON.parse(factoryContractArtifact);
+
+    const factoryContractAbi = factoryContract["abi"];
+    const factoryContractBytecode = factoryContract["bytecode"];
+
+    const deployFactoryData = encodeDeployData({
+        abi: factoryContractAbi,
+        bytecode: factoryContractBytecode,
+        args: [PUBLIC_KEY],
+    })
+
+    const deployFactoryEstimatedGas = await publicClient.estimateGas({
+        account,
+        data: deployFactoryData,
+    })
+
+    console.log("deploy factory gas estimate: ", formatUnits(deployFactoryEstimatedGas, 18))
+
+    const TOKEN_1_ADDRESS = "0x9D173E6c594f479B4d47001F8E6A95A7aDDa42bC"
+    const TOKEN_2_ADDRESS = "0xfb5B838b6cfEEdC2873aB27866079AC55363D37E"
+
+    const createPairEstimatedGas = await publicClient.estimateContractGas({
+        account,
+        abi: factoryContractAbi,
+        address: BNB_FACTORY_ADDRESS,
+        functionName: 'createPair',
+        args: [TOKEN_1_ADDRESS, TOKEN_2_ADDRESS]
+    });
+
+    console.log("create pair gas estimate: ", formatUnits(createPairEstimatedGas, 18));
+
+    const routerContractPath = path.join(ARTIFACTS_PATH, "PancakeRouter.sol/PancakeRouter.json")
     const routerContractArtifact = await fs.readFile(routerContractPath, "utf-8")
 
     const routerContract = JSON.parse(routerContractArtifact);
@@ -37,31 +73,59 @@ async function estimateSwapContractsGas() {
     const routerContractAbi = routerContract["abi"];
     const routerContractBytecode = routerContract["bytecode"];
 
-    const deployData = encodeDeployData({
+    const deployRouterData = encodeDeployData({
         abi: routerContractAbi,
         bytecode: routerContractBytecode,
-        args: [PUBLIC_KEY],
+        args: [BNB_FACTORY_ADDRESS, BNB_WETH_ADDRESS],
     })
 
-    const deployEstimatedGas = await publicClient.estimateGas({
+    const deployRouterEstimatedGas = await publicClient.estimateGas({
         account,
-        data: deployData,
-    })
-
-    console.log("deploy gas estimate: ", formatUnits(deployEstimatedGas, 18))
-
-    const TOKEN_1_ADDRESS = "0x9D173E6c594f479B4d47001F8E6A95A7aDDa42bC"
-    const TOKEN_2_ADDRESS = "0xfb5B838b6cfEEdC2873aB27866079AC55363D37E"
-
-    const createPairEstimatedGas = await publicClient.estimateContractGas({
-        account,
-        abi: routerContractAbi,
-        address: BNB_FACTORY_ADDRESS,
-        functionName: 'createPair',
-        args: [TOKEN_1_ADDRESS, TOKEN_2_ADDRESS]
+        data: deployRouterData,
     });
 
-    console.log("create pair gas estimate: ", formatUnits(createPairEstimatedGas, 18));
+    console.log("deploy router gas estimate: ", formatUnits(deployRouterEstimatedGas, 18) )
+
+    const SWAP_TOKEN_1_ADDRESS = "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d"
+    const SWAP_TOKEN_2_ADDRESS = "0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82"
+
+    const amountIn = parseUnits('0.1', 18);
+    const amountOutMin = parseUnits('0.04', 18);
+    const swapPath = [SWAP_TOKEN_1_ADDRESS, SWAP_TOKEN_2_ADDRESS];
+
+    const approveResult = await client.writeContract({
+        address: SWAP_TOKEN_1_ADDRESS,
+        abi: erc20Abi,
+        functionName: 'approve',
+        args: [BNB_ROUTER_ADDRESS, amountIn]
+    })
+
+    const approveRx = await publicClient.waitForTransactionReceipt({ hash: approveResult, confirmations: 1 });
+
+    console.log("token approve gas used: ", formatUnits(approveRx.gasUsed, 18))
+
+    const deadline = Math.floor(Date.now() / 1000) + 60 * 10;
+
+    const callSwapEstimatedGas = await publicClient.estimateContractGas({
+        account,
+        abi: routerContractAbi,
+        address: BNB_ROUTER_ADDRESS,
+        functionName: 'swapExactTokensForTokens',
+        args: [
+            amountIn,
+            amountOutMin,
+            swapPath,
+            PUBLIC_KEY,
+            deadline
+        ]
+    });
+
+    console.log("swap tokens gas estimate: ", formatUnits(callSwapEstimatedGas, 18));
+
+    const totalGasBigInt = deployFactoryEstimatedGas + createPairEstimatedGas + deployRouterEstimatedGas + approveRx.gasUsed + callSwapEstimatedGas;
+    const totalGas = formatUnits(totalGasBigInt, 18);
+
+    console.log(`TCO (Swap Tokens): ${totalGas}`)
 }
 
 estimateSwapContractsGas().catch(console.error)
